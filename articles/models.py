7387d4ec5a0f046b79fb93b8424232c958aa4f25
shankar <15pa1a1249@vishnu.edu.in>
2018-03-05 19:08:26 +0530

    'create_form_working'

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,9 +8,8 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-
-    #thumbnail
-    #author
+    thumb = models.ImageField(default='default.png',blank=True)
+    author = models.ForeignKey(User , default=None,on_delete=None)
 
     def __str__(self):
         return self.title
--- articles/urls.py
+++ articles/urls.py
@@ -18,6 +18,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('',views.article_list),
+    path('',views.article_list, name = "list"),
+    path('create/',views.article_create, name = 'create'),
+    path('<slug>',views.article_details , name = "detail"),
+
 ]
